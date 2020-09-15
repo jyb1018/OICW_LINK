f53bf5f0d2c8473b000532ae500b8778e538fb30
jyb1018 <43407224+jyb1018@users.noreply.github.com>
2020-09-15 16:37:42 +0900

    Create boj_1992.cpp

---
+++ 정유빈/중급/5주차/boj_1992.cpp
@@ -0,0 +1,73 @@
+//틀렸다는데 반례를 못찾겠는데;
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int arr[64][64];
+int N;
+
+int filledWhole(int x, int y, int size)
+{
+	cout << size << endl;
+	int prev = arr[x][y];
+	for(int i = x; i<x+size; i++)
+		for(int j = y; j<y+size; j++)
+		{
+			cout << "i : " << i << " j : " << j <<endl;
+			if(prev != arr[i][j])
+				return -1;
+			
+		}
+			return prev;
+	
+	
+}
+	
+string compress(int fx, int fy, int size)
+{
+	if(size == 1)
+		return to_string(arr[fx][fy]);
+	int t = filledWhole(fx, fy, size);
+
+	if(t == 1)
+		return "1";
+	else if(t == 0)
+		return "0";
+	
+	string temp = "(";
+	temp += compress(fx, fy, size/2);
+	temp += compress(fx, fy + size/2, size/2);
+	temp += compress(fx + size/2, fy, size/2);
+	temp += compress(fx + size/2, fy + size/2, size/2);
+	
+	return temp + ")";
+	
+}
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	freopen("input.txt", "r", stdin);
+	
+	cin >> N;
+	for(int i = 0; i<N; i++)
+	{
+		string temp;
+		cin >> temp;
+		for(int j = 0; j<N; j++)
+			arr[i][j] = temp.at(j)-'0';
+		
+	}
+	
+	cout << compress(0,0,N);
+	
+
+	
+}
